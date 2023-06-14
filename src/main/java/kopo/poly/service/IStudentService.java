c5489534eb6b8c95df454881cb6d58f7e18f1f81
Geuniv <kkoowl99@gmail.com>
2023-06-14 19:10:54 +0900

    2023.06.14 잔디 심기 ( 수업 Spring + DB 연결 예제 )

--- src/main/java/kopo/poly/service/IStudentService.java
+++ src/main/java/kopo/poly/service/IStudentService.java
@@ -15,6 +15,16 @@ public interface IStudentService {
 
     List<StudentDTO> insertStudent(StudentDTO pDTO) throws Exception;
 
+    List<StudentDTO> insertStudentList(List<StudentDTO> pDTO) throws Exception;
+
     /* DELETE 기능 구현 */
     List<StudentDTO>deleteStudent(StudentDTO pDTO) throws Exception;
+
+    /**
+     * 학생 수정한 뒤, 결과 조회하기
+     *
+     * @param pDTO 수정할 학생 정보를 가지고 있는 DTO
+     * @return DB 조회한 학생 정보
+     * */
+    List<StudentDTO> updateStudent(StudentDTO pDTO) throws Exception;
 }
--- src/main/java/kopo/poly/service/impl/StudentService.java
+++ src/main/java/kopo/poly/service/impl/StudentService.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -47,20 +48,100 @@ public class StudentService implements IStudentService {
         return rList;
     }
 
-    /* DELETE 기능 */
+    @Override
+    public List<StudentDTO> insertStudentList(List<StudentDTO> pList) throws Exception {
+
+        log.info(this.getClass().getName() + ".insertStudentList Start !");
+
+        pList.forEach(dto -> {
+            try {
+                this.insertStudent(dto);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Iterator<StudentDTO> it = pList.iterator();
+        List<StudentDTO> rList = new ArrayList<>();
+
+        while (it.hasNext()) {
+
+            Optional<StudentDTO> res = Optional.ofNullable(
+                    studentMapper.getStudent(it.next()) /* SELECT 쿼리 */
+            );
+
+            /* INSERT 쿼리 */
+            if (!res.isPresent()) { // DB 조회 결과로 회원아이디가 존재하지 않는다면...
+                studentMapper.insertStudent(it.next()); // 학생 등록 SQL 실행하기, ( INSERT 쿼리 )
+            } else {
+                 log.info("조회 가능");
+            }
+            rList = Optional.ofNullable(studentMapper.getStudentList()).orElseGet(ArrayList::new);
+        }
+        /* SELECT 쿼리 */
+        /* Student 테이블에 등록된 학생 아이디가 존재하는지 체크하기 위해 DB 조회하기 ( Optional ) */
+
+        log.info(this.getClass().getName() + ".insertStudentList End !");
+
+        return rList;
+    }
+
+    @Override
+    public List<StudentDTO> updateStudent(StudentDTO pDTO) throws Exception {
+
+        log.info(this.getClass().getName() + ".updateStudent Start !");
+
+        /* SELECT 쿼리 */
+        /* Student 테이블에 등록된 학생 아이디가 존재하는지 체크하기 위해 DB 조회하기 ( Optional ) */
+        Optional<StudentDTO> res = Optional.ofNullable(
+                studentMapper.getStudent(pDTO) /* SELECT 쿼리 */
+        );
+
+        /* INSERT 쿼리 */
+        if (res.isPresent()) { // DB 조회 결과로 회원아이디가 존재하지 않는다면...
+            studentMapper.updateStudent(pDTO);
+            log.info(pDTO.getUserId() + "님이 수정되었습니다.");// 학생 등록 SQL 실행하기, ( INSERT 쿼리 )
+        } else {
+            log.info("회원이 존재하지 않아 수정되지 못했습니다.");
+
+        }
+
+        /* SELECT 쿼리 */
+        /* 학생 테이블 전체 조회하기 ( Optional ) */
+        List<StudentDTO> rList = Optional.ofNullable(
+                studentMapper.getStudentList() /* SELECT 쿼리 */
+        ).orElseGet(ArrayList::new); // null을 empty로 변환
+
+        log.info(this.getClass().getName() + ".insertStudent End !");
+
+        return rList;
+    }
+
+    /* DELETE 기능 ( 과제 ) */
+    /**
+     * 학생 삭제한 뒤, 결과 조회하기
+     *
+     * @param pDTO 삭제하기 위해 학생 아이디 정보를 가지고 있는 DTO
+     * @return DB 조회한 학생 정보
+     * */
     @Override
     public List<StudentDTO> deleteStudent(StudentDTO pDTO) throws Exception {
 
         log.info(this.getClass().getName() + ".deleteStudent Start !");
 
         /* SELECT 쿼리 */
+        /* Student 테이블에 등록된 학생 아이디가 존재하는지 체크하기 위해 DB 조회하기 */
         Optional<StudentDTO> res = Optional.ofNullable(
                 studentMapper.getStudent(pDTO)
         );
 
         /* DELETE 쿼리 */
-        if (res.isPresent()) {
-            studentMapper.deleteStudent(pDTO);
+        if (res.isPresent()) { // DB 조회 결과로 회원아이디가 존재한다면..
+            studentMapper.deleteStudent(pDTO); // 학생 삭제 SQL 실행하기
+            log.info(pDTO.getUserId() + "님이 삭제되었습니다.");
+        } else {
+            log.info("회원이 존재하지 않아 삭제되지 못했습니다.");
+
         }
 
         /* SELECT 쿼리 */
